93f600b010bb00b3eb287a3ffb14b3ae897ec4ea
arsheel <sheikharsheel@gmail.com>
2023-04-11 02:50:06 +0530

    added index and chat pages

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
-export default function Chats() {
-  return <div className="background">chats</div>;
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
+export default function Home() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setShowChat(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (username === "" || secret === "") {
+      router.push("/");
+    }
+  }, [username, secret]);
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 212px)"
+          projectID="4bac28be-d2c3-4b17-b58b-95b2394e75da"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
 }
